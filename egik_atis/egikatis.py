0e224306523898c0c247a2ebdd7cdc0fc9984438
Samet İşler <islersamet95@gmail.com>
2023-03-15 20:50:01 +0300

    v2 updates.

--- egik_atis/egikatis.py
+++ egik_atis/egikatis.py
@@ -1,19 +1,28 @@
+## Eğik Atış Projesi_v2
+
 import math
 is_on = True
 
-while is_on:
-    speed = float(input("Lütfen bir hiz değeri giriniz."))
-    angle = float(input("Lütfen ° cinsinden bir eğim acisi değeri giriniz."))
-    g = 10
-    def egik_atis(v = speed,a = angle):
-        menzil = ((v**2)/g) * (math.sin(math.radians(2*angle)))
-        t_ucus = (v*(math.sin(math.radians(angle))))/ g * 2
-        hmax = (g * ((t_ucus/2)**2)/2)
-        print(f"{speed}m/s hızı ve {angle}° açısı ile atılan cismin;\nMaksimum menzili {round(menzil,2)}m.'dir.\nUçuş süresi {round(t_ucus,2)}s.'dir.\nMaksimum yüksekliği ise {round(hmax,2)}m'dir.")
-    egik_atis()
-    cont = input("Yeni bir hesaplama yapmak ister misiniz ?(evet / hayır)")
+def egik_atis(v, a):
+    """
+    Bu fonksiyon aşağıdaki parametreleri kullanir:
+    v = hiz,
+    a = aci
+    Fonksiyon bu verilerle eğik atis yapilan bir cismin maksimmum yüksekliğini, menzilini ve ucus suresini hesaplar.
+    """
+    g = 10 # g degeri 10 olarak kabul edilmiştir.
+    menzil = ((v**2)/g) * (math.sin(math.radians(2*a))) # maksimum menzili hesaplar.
+    t_ucus = (v*(math.sin(math.radians(a))))/ g * 2 # ucus suresini hesaplar.
+    hmax = (g * ((t_ucus/2)**2)/2) # maksimum yuksekligi hesaplar.
+    print(f"{v}m/s hizi ve {a}° acisi ile atilan cismin;\nMaksimum menzili {round(menzil,2)}m.'dir.\nUcus süresi {round(t_ucus,2)}s.'dir.\nMaksimum yuksekliği ise {round(hmax,2)}m'dir.")
+
+while is_on: # Bu dongu ile kullanıcıya istedigi kadar hesaplama yapma sansi taninmistir.
+    speed = float(input("Lütfen bir hiz değeri giriniz.")) # Veri girisi.
+    angle = float(input("Lütfen ° cinsinden bir egim acisi değeri giriniz.")) # Veri girisi.
+    egik_atis(speed,angle)
+    cont = input("Yeni bir hesaplama yapmak ister misiniz ?(evet / hayir)") # Kullaniciya yeni bir hesaplama yapmak isteyip istemedigi sorulmaktadir.
     if cont == "evet":
         is_on = True
     else:
-        is_on = False
-        print("Hoşçakalın.")
+        print("Hoscakalin.")
+        break
--- empedans calc/emp_calc.py
+++ empedans calc/emp_calc.py
@@ -1,15 +1,32 @@
-import math
+## Empedans Projesi_v2
 
-volt = float(input("Lütfen voltaj değerini giriniz."))
-hertz = float(input("Lütfen hertz değerini giriniz."))
-direnc = float(input("Lütfen direnç değerini giriniz."))
-fahrad = float(input("Lütfen kondansatör değerini giriniz."))
-henry = float(input("Lütfen bobin değerini giriniz."))
+import math #math kutuphanesi yuklenmistir.
 
-def emp(v = volt, h= hertz, r = direnc, f = fahrad, b = henry):
-    xl = 2*(math.pi)*h*b
-    xc = (2*(math.pi)*h*f)**-1
-    z = complex(r,(xl-xc))
-    print(round(abs(z),2))
+print("Empedans hesaplamak için lütfen istenen değerleri giriniz.")
+
+volt = float(input("Lütfen voltaj değerini giriniz.")) # Veri girişi.
+hertz = float(input("Lütfen hertz değerini giriniz.")) # Veri girişi.
+direnc = float(input("Lütfen direnç değerini giriniz.")) # Veri girişi.
+fahrad = float(input("Lütfen kondansatör değerini giriniz.")) # Veri girişi.
+henry = float(input("Lütfen bobin değerini giriniz.")) # Veri girişi.
+
+def emp(v= volt, h= hertz, r= direnc, f= fahrad, b= henry): # Yalnızca seri bağlı devrelerde empedans hesabı yapan bir fonksiyon tanımlama.
+    """
+    The parameters of the function is defined below:
+    v = volt,
+    h = hertz,
+    r = resistance
+    b = inductance
+    This function gets the v, h, r, f, b values and returns impedance of the circuit...
+    """
+    xl = 2*(math.pi)*h*b # Endüktif reaktans hesaplar.
+    xc = (2*(math.pi)*h*f)**-1 # Kapasitif reaktans hesaplar.
+    z = complex(r,(xl-xc)) # Toplam empedans hesaplar
+    print(round(abs(z),2)) # Sonucu 2 ondalıklı olarak ekrana yazar.
     
-emp()
+emp(volt, hertz, direnc, fahrad, henry) # Öncelikle kodu  emp(v, h, r, f, b) yazdım ve hata aldım. 
+# Biraz araştırma yaptıktan sonra parametrelere atanan değişkenleri buraya emp() içine gerektiğini farkettim ve düzelttim.
+
+
+
+
